4a1bd1fdb9af797f5e64c400d25437f6eb443ee4
Djordje Jovanovic <jovanovic.djo@gmail.com>
2025-09-19 22:37:59 +0200

    update logic for execution

--- driver/driver_manager.py
+++ driver/driver_manager.py
@@ -1,20 +1,22 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 
 def create_driver():
-    driver = Options()
-    driver.add_argument('--no-sandbox')
-    driver.add_argument('--disable-dev-shm-usage')
-    driver.add_argument('--disable-gpu')
-    driver.add_argument('--headless')
+    options = Options()
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--headless')
 
     try:
-        driver = webdriver.Chrome(options=driver)
+        service = Service(ChromeDriverManager().install())
+        driver = webdriver.Chrome(service=service, options=options)
         return driver
     except Exception as e:
         print(f"Error setting up Chrome WebDriver: {e}")
         return None
-    
     
     
 
--- script.py
+++ script.py
@@ -7,8 +7,16 @@ from datetime import datetime
 from typing import List, Dict
 from selenium.common.exceptions import WebDriverException
 from dotenv import load_dotenv
+from driver import driver_manager
 
 load_dotenv()
 website_url = os.getenv("WEBSITE_URL")
 
-print(website_url)
+random_number = random.randint(5, 2000)
+for _ in range(7):
+    driver = driver_manager.create_driver()
+    if driver:
+        driver.get(website_url)
+        driver.quit()
+    else:
+        print("Driver could not be created")

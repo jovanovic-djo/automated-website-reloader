1dfcc294b6ad38ecf7c7db14b7f0871b7091eda5
Djordje Jovanovic <jovanovic.djo@gmail.com>
2025-09-20 10:49:36 +0200

    update script.py and add content management to driver

--- driver/driver_manager.py
+++ driver/driver_manager.py
@@ -5,10 +5,17 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 def create_driver():
     options = Options()
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    options.add_argument('--disable-gpu')
-    options.add_argument('--headless')
+    # options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--no-sandbox")
+    prefs = {
+    # "profile.managed_default_content_settings.images": 2,
+    "profile.managed_default_content_settings.stylesheets": 2,
+    "profile.managed_default_content_settings.javascript": 2,
+    }   
+    options.add_experimental_option("prefs", prefs)
 
     try:
         service = Service(ChromeDriverManager().install())
--- script.py
+++ script.py
@@ -1,22 +1,30 @@
 import os
 import time
 import random
-import sys
-from threading import Thread
-from datetime import datetime
-from typing import List, Dict
-from selenium.common.exceptions import WebDriverException
 from dotenv import load_dotenv
 from driver import driver_manager
 
 load_dotenv()
 website_url = os.getenv("WEBSITE_URL")
 
+timer = time.localtime()
+
 random_number = random.randint(5, 2000)
-for _ in range(7):
-    driver = driver_manager.create_driver()
-    if driver:
+driver = driver_manager.create_driver()
+start_time = time.perf_counter()
+
+if driver:
+    try:
         driver.get(website_url)
+
+        for _ in range(1, random_number):
+            driver.refresh()
+    finally:
         driver.quit()
-    else:
-        print("Driver could not be created")
+else:
+    print("Driver could not be created")
+    
+end_time = time.perf_counter()
+
+elapsed_time = end_time - start_time
+print(f"Elapsed time: {elapsed_time:.4f} seconds")
